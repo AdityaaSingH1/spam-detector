39f54e52b4bcb82c17682cc4c69ad7d2e4476a31
aditya singh <singh.adi0409@gmail.com>
2026-04-14 19:04:47 +0530

    fixed requirements and filename

--- project.py
+++ project.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import streamlit as st
+
 readdata = pd.read_csv(
     'spam.csv', encoding='latin-1')
 print(readdata)
